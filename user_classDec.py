ff95b227964735b1449c2063aa97fbd1ef819430
T0McKay <mckay0534@hotmail.com>
2025-10-27 09:49:21 +0000

    Further User Implementation
    Bug fixing and making UI more intuitive

--- user_classDec.py
+++ user_classDec.py
@@ -9,29 +9,41 @@ class User:
         self.password = password
         User.ListOfUsers[userID] = self #adding self to inventory instead of password for encapsulation
 
+    #due to encapsulation, password can only be accessed through this method
+    #makes password protection slightly safer
     def validatePassword(self, queriedPassword):
         if queriedPassword == self.password:
             return True
         else:
             return False
 
+    #used at start of program to get all known users out of CSV to be used in runtime
     @classmethod
     def initialiseListOfUsers(cls):
         if os.path.getsize("users.csv") > 0: #makes sure csv is not empty
+            #reading from CSV:
             with open("users.csv", "r", newline="") as csvfile:
                 csvreader = csv.reader(csvfile, delimiter=',')
                 #initialises each row as an object for use
                 for row in csvreader:
                     savedUser = User(row[0], row[1])
 
+    #allows user object to be added at runtime through user input
     @classmethod
     def addUserByInput(cls):
+        print("----------------")
+        print("--- ADD USER ---")
+        print("----------------")
+        print("")
         userID = input("Enter new user ID: ")
         password = input("Enter new user password: ")
         newUser =User(userID, password)
         with open("users.csv", "a", newline="") as csvfile:
             writingToFile = csv.writer(csvfile)
-            writingToFile.writerow(newUser.__dict__.values())
+            writingToFile.writerow(newUser.__dict__.values()) #tranforms object into dictionary for writing to CSV
+        print("")
+        print("User added successfully")
+        print("")
 
     #used to log in - checks userID and returns true if matches parameter
     @classmethod # this function belongs to the class not an individual user object
